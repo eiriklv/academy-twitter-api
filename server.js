48860042bc88fe4e1a6072302760b3a49492cbaf
Eirik Vullum <evullum@gmail.com>
2021-04-21 12:42:45 +0200

    add health check route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello from Twitter API!' })
+});
+
 app.get('/tweets', async (req, res) => {
   const tweets = await getTweets();
   res.send(tweets);
